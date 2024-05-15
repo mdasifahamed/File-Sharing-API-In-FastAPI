1a1857c207156c4f8a09abf1c9d872465ae09b2f
mdasifahamed <mdasifamahed89@yahoo.com>
2024-05-15 15:12:15 +0600

    Refactored

--- test/mocks/test_download.py
+++ test/mocks/test_download.py
@@ -12,7 +12,8 @@ def test_share_a_file(user1_file_upload,user1_logged_in,test_user2):
     file_info = user1_file_upload
     res = user1_logged_in.post(f'/sharefile',json={'file_name':file_info['file_name'],'shared_to':test_user2['email']})
     assert res.status_code == 201
-    return res.json()
+
+
 def test_shared_file_download(user2_logged_in):
     res = user2_logged_in.get('/getSharedFiles')
     files = res.json()
